83bc7d607e73ac74a6d438abff45d61e468d192a
GaudRavi09 <gaudravi09@gmail.com>
2025-06-14 16:07:27 +0530

    feat: added resume page with downloadable PDF support

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { About } from './pages/about/about';
+import { Resume } from './pages/resume/resume';
 
 export const routes: Routes = [
   { path: '', component: About },
+  { path: 'resume', component: Resume },
   { path: 'about', redirectTo: '', pathMatch: 'full' },
 ];
