18bc7566f3b16a08ee4f41717534f8ea816b3719
GaudRavi09 <gaudravi09@gmail.com>
2025-06-14 16:20:47 +0530

    feat: added contact page with basic layout and details

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { About } from './pages/about/about';
 import { Resume } from './pages/resume/resume';
+import { Contact } from './pages/contact/contact';
 
 export const routes: Routes = [
   { path: '', component: About },
   { path: 'resume', component: Resume },
+  { path: 'contact', component: Contact },
   { path: 'about', redirectTo: '', pathMatch: 'full' },
 ];
