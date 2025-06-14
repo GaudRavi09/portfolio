9fa2c317ed09e8f0ef512ed679e2bf0daadffe9b
GaudRavi09 <gaudravi09@gmail.com>
2025-06-14 17:05:28 +0530

    chore: added @angular/fire for firebase integration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,8 @@
 import { routes } from './app.routes';
 import { provideRouter } from '@angular/router';
+import { environment } from '../environments/environment';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 
@@ -8,6 +11,8 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideZonelessChangeDetection(),
     provideBrowserGlobalErrorListeners(),
+    provideFirestore(() => getFirestore()),
     provideClientHydration(withEventReplay()),
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
   ],
 };
