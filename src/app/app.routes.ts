e9681daee502715a927ec4ce7d5748344c10ff60
GaudRavi09 <gaudravi09@gmail.com>
2025-06-14 13:37:25 +0530

    feat: added about page and configure it as the default route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { About } from './pages/about/about';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: About },
+  { path: 'about', redirectTo: '', pathMatch: 'full' },
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { TitleCasePipe } from '@angular/common';
 import { Sidebar } from './components/sidebar/sidebar';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   styleUrl: './app.scss',
   templateUrl: './app.html',
-  imports: [Sidebar, RouterLink, RouterOutlet, TitleCasePipe],
+  imports: [Sidebar, RouterLink, RouterOutlet, TitleCasePipe, RouterLinkActive],
 })
 export class App {}
