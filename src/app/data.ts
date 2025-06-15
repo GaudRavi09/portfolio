4d97d69583035a762f984cbf85e347dd5c27155e
GaudRavi09 <gaudravi09@gmail.com>
2025-06-15 16:42:42 +0530

    feat: implement ui for showcasing projects

--- src/app/data.ts
+++ src/app/data.ts
@@ -13,3 +13,31 @@ export const profile = {
     facebook: 'https://www.facebook.com/GaudRavi09',
   },
 };
+
+export const projects = [
+  {
+    name: 'CardioVisual',
+    image: 'https://picsum.photos/200/150',
+    downloads: '100k',
+    appStoreLink: 'https://apps.apple.com/us/app/cardiovisual-heart-health/id1009028152',
+    subHeading: 'Health Education Application',
+    playStoreLink: 'https://play.google.com/store/apps/details?id=com.cardiovisual.app',
+  },
+  {
+    name: 'Rewaa POS',
+    image: 'https://picsum.photos/200/150',
+    downloads: '6k',
+    subHeading: 'Retail Management Software',
+    playStoreLink: 'https://play.google.com/store/apps/details?id=com.rewaatech.pos',
+    appStoreLink:
+      'https://apps.apple.com/us/app/%D9%86%D9%82%D8%A7%D8%B7-%D8%A7%D9%84%D8%A8%D9%8A%D8%B9-%D9%85%D9%86-%D8%B1%D9%88%D8%A7%D8%A1-rewaa-pos/id1658257539',
+  },
+  {
+    name: 'MyGolf2u',
+    image: 'https://picsum.photos/200/150',
+    downloads: '10k',
+    subHeading: 'A Better Golfing Experience',
+    playStoreLink: 'https://play.google.com/store/apps/details?id=com.brite.mygolf2u&gl=US',
+    appStoreLink: 'https://apps.apple.com/us/app/mygolf2u/id1092380543',
+  },
+];
